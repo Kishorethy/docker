f10c4261381aa10d3901a1f55fc730806582e665
kishorethy <kishoresureshks@gmail.com>
2025-02-28 16:39:09 +0530

    v1

--- src/main/java/com/example/Helloworld.java
+++ src/main/java/com/example/Helloworld.java
@@ -4,6 +4,7 @@ package com.example;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("webhook triggered");
     }
 }
 
